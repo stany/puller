4b5a0a5ce921cf034e53b20a5126d620ee2c4a1e
Marcin Stankiewicz <stankiewicz.forward@gmail.com>
2012-05-18 15:45:16 +0200

    rails app initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puller::Application.config.secret_token = '60351c76881a1c3993a5687374e0820e57e4ae808faa9270231c1f8fe82ee29021d3a3493551acc7d004f59e57d9c95b3d94c384bcd428255771f4fe22ade55f'
